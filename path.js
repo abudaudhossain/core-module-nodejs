99fe74aebc268da2f4981be56ad55b735b17e4f4
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-08-08 08:01:27 +0600

    add path core module in nodejs

---
+++ path.js
@@ -0,0 +1,8 @@
+const path = require("path");
+
+// const myPath = path.basename('F:/web Disign/JavaScript/nodejs/core-module-nodejs/path.js');
+let myPath = path.dirname('F:/web Disign/JavaScript/nodejs/core-module-nodejs/path.js');
+myPath = path.extname('F:/web Disign/JavaScript/nodejs/core-module-nodejs/path.js');
+myPath = path.parse('F:/web Disign/JavaScript/nodejs/core-module-nodejs/path.js');
+console.log(myPath);
+ 
